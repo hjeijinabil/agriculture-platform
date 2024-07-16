dd5aa347ae1a5410d8903032730d5eddf7308844
=hjeijinabil <=nabilhjeiji@gmail.com>
2024-07-16 02:43:41 +0200

    add crud Crop

--- Farm-Management/src/main/java/com/agriculture_platform/Farm/Management/Controller/CropController.java
+++ Farm-Management/src/main/java/com/agriculture_platform/Farm/Management/Controller/CropController.java
@@ -1,4 +1,52 @@
 package com.agriculture_platform.Farm.Management.Controller;
 
+import com.agriculture_platform.Farm.Management.Entity.Crop;
+import com.agriculture_platform.Farm.Management.Entity.Farm;
+import com.agriculture_platform.Farm.Management.Service.CropService;
+import com.agriculture_platform.Farm.Management.Service.FarmService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
 public class CropController {
+    @Autowired
+    private CropService cropService;
+
+
+    @GetMapping
+    public ResponseEntity<List<Crop>> getAllCrops() {
+        List<Crop> crops = cropService.getAllCrops();
+        return new ResponseEntity<>(crops, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Crop> getCropById(@PathVariable Long id) {
+        Crop crop = cropService.getCropById(id);
+        if (crop != null) {
+            return new ResponseEntity<>(crop, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+    @PostMapping
+    public ResponseEntity<Crop> addCrop(@RequestBody Crop crop) {
+        Crop newCrop = cropService.addCrop(crop);
+        return new ResponseEntity<>(newCrop, HttpStatus.CREATED);
+    }
+
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCrop(@PathVariable Long id) {
+        cropService.deleteCrop(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
--- Farm-Management/src/main/java/com/agriculture_platform/Farm/Management/Service/CropService.java
+++ Farm-Management/src/main/java/com/agriculture_platform/Farm/Management/Service/CropService.java
@@ -1,8 +1,10 @@
 package com.agriculture_platform.Farm.Management.Service;
 
 import com.agriculture_platform.Farm.Management.Entity.Crop;
+import com.agriculture_platform.Farm.Management.Entity.Farm;
 import com.agriculture_platform.Farm.Management.Exception.ResourceNotFoundException;
 import com.agriculture_platform.Farm.Management.Repository.CropRepository;
+import com.agriculture_platform.Farm.Management.Repository.FarmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +15,25 @@ public class CropService {
     @Autowired
     private CropRepository cropRepository;
 
+
+
+    public List<Crop> getAllCrops() {
+        return cropRepository.findAll();
+    }
+    public Crop addCrop(Crop crop) {
+        return cropRepository.save(crop);
+    }
+
+
+
+
+
+    public Crop getCropById(Long id) {
+        return cropRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Crop not found"));
+    }
+
+    public void deleteCrop(Long id) {
+        cropRepository.deleteById(id);
+    }
 
 }
